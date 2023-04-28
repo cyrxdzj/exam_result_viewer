a55f713540748da22ffd2a0aac719bf94f5eb2f8
残阳如血 <2824671438@qq.com>
2023-04-28 20:21:33 +0800

    写了主页。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {Page, Card, NextLine, Text} from "./Components";
+import {Button} from "antd";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Page>
+            <Card>
+                <center><Text type={"h1"}>ExamResultViewer</Text></center>
+                <center><Text>学业水平质量测试结果分析器</Text></center>
+                <Text type={"h2"}>生成数据源文件</Text>
+                <NextLine/>
+                <Text>ExamResultViewer并不能智能地识别Excel中的内容，因此需要您先将Excel文件转换为数据源文件再进行分析。数据源文件包含了您的配置，这可以使您不必每次分析都要重新配置。</Text>
+                <NextLine/>
+                <Button type={"primary"}>开始生成</Button>
+                <NextLine/>
+                <Text type={"h2"}>分析数据源文件</Text>
+                <NextLine/>
+                <Text>配置好数据源文件后，您可以将其导入这里以进行分析。</Text>
+                <NextLine/>
+                <Button type={"primary"}>开始分析</Button>
+                <NextLine/>
+            </Card>
+        </Page>
+    );
 }
 
 export default App;
---
+++ src/Components.js
@@ -0,0 +1,87 @@
+import {Table} from "antd";
+
+const text_size = {
+    "normal": "16px",
+    "h4": "20px",
+    "h3": "24px",
+    "h2": "32px",
+    "h1": "40px"
+};
+
+export function Background({children}) {
+    return (<div style={{
+        "background": "linear-gradient(#0ba360 0%, #3cba92 100%)",
+        "position": "fixed",
+        "padding": "30px",
+        "top": "0px",
+        "bottom": "0px",
+        "left": "0px",
+        "right": "0px",
+        "overflow": "auto"
+    }}>{children}</div>);
+}
+
+export function Card({children}) {
+    return (<div style={{
+        "background": "rgba(255,255,255,0.5)",
+        "padding": "30px",
+        "borderRadius": "30px"
+    }}>{children}</div>);
+}
+
+export function Page({children}) {
+    const thanks_table_data = [
+        {
+            "key": "antd",
+            "name": <Text>Ant Design</Text>,
+            "usage": <Text>页面组件设计，如按钮、输入框等。</Text>,
+            "url": <Text>https://ant.design/</Text>
+        },
+        {
+            "key": "lxgw",
+            "name": <Text>霞鹜文楷</Text>,
+            "usage": <Text>页面字体。</Text>,
+            "url": <Text>https://github.com/lxgw/LxgwWenKai</Text>
+        }
+    ];
+    const columns = [
+        {
+            title: <Text bold={true}>名称</Text>,
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: <Text bold={true}>用途</Text>,
+            dataIndex: 'usage',
+            key: 'usgae',
+        },
+        {
+            title: <Text bold={true}>URL</Text>,
+            dataIndex: 'url',
+            key: 'url'
+        }
+    ];
+    return (<Background>
+        {children}
+        <NextLine size={"30px"}/>
+        <Card>
+            <Text>ExamResultViewer确保所有数据本地分析，不会上传至服务器。</Text>
+            <NextLine/>
+            <Text>以下是ExamResultViewer所使用的部分开源软件，在此表示感谢。</Text>
+            <NextLine/>
+            <Table dataSource={thanks_table_data} columns={columns} pagination={false}/>
+        </Card>
+    </Background>);
+}
+
+export function Text({children, type = "normal", bold = false}) {
+    return (<span style={{
+        "fontSize": text_size[type],
+        "fontFamily": "霞鹜文楷",
+        "fontWeight": (type.startsWith("h") || bold) ? "bold" : "normal"
+    }}>{children}</span>);
+}
+
+export function NextLine({size = "8px"}) {
+    return (<div style={{"display": "block", "height": size}}/>);
+}
